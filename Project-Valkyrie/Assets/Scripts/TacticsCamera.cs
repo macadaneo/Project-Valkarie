f7861faeb7dd34f6f3dea068a2e406699ee09697
Dane Ericksen <10721110@my.uvu.edu>
2020-06-28 17:19:17 -0600

    Created Test Map, Tactical Movement System Scripts, & Tile system

---
+++ Project-Valkyrie/Assets/Scripts/TacticsCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsCamera : MonoBehaviour
+{
+   public void Rotateleft()
+   {
+      transform.Rotate(Vector3.up, 90, Space.Self);
+   }
+
+   public void RotateRight()
+   {
+       transform.Rotate(Vector3.up, -90, Space.Self);
+   }
+}
---
+++ Project-Valkyrie/Assets/Scripts/TacticsMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsMove : MonoBehaviour
+{
+    List<Tile> selectableTiles = new List<Tile>();
+    private GameObject[] tiles;
+    
+    Stack<Tile> path = new Stack<Tile>();
+    
+    public int move = 5;
+    public float jumpHeight = 2;
+    public float moveSpeed = 2;
+    
+    Vector3 velocity = new Vector3();
+    Vector3 heading = new Vector3();
+    protected void Init()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+        
+        
+    }
+    
+    
+}
