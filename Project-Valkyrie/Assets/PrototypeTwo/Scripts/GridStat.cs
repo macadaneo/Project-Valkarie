1bcb9599d8f56608d8b52b89f27f82f8a5bbd78c
Dane Ericksen <10721110@my.uvu.edu>
2020-06-30 21:18:47 -0600

    prototyped a new grid system/map creation

---
+++ Project-Valkyrie/Assets/PrototypeTwo/Scripts/GridStat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridStat : MonoBehaviour
+{
+    public int visited = -1;
+    public int x = 0;
+    public int y = 0;
+
+    
+
+   // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
