6bc1500107ddc9d7bdefcd82fc97d943cdd8589a
Dane Ericksen <10721110@my.uvu.edu>
2020-08-03 22:25:57 -0600

    Review on variables, methods, locals and globals.

---
+++ Project-Valkyrie/Assets/Learning Unity B/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    private int number;
+    private int counter;
+    
+    private string myName;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 1;
+        
+        myName = "Dane";
+        
+        print("Hello " + myName + ", your number is " + number);
+        
+        //-------------------------------^^ Test Code ^^-------------------------------------
+        //-------------------------------vv Counter vv---------------------------------------
+
+        int localVariable = 3;
+        
+        print("local variable: " + localVariable);
+
+        counter = 0;
+        
+        myFirstMethod();
+        
+        mySecondMethod("Magnus");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // print(myName);
+
+       counter++;
+       
+      // print("counter = " + counter);
+    }
+
+
+    public void myFirstMethod()
+    {
+        print("Hello World!");
+    }
+
+    public void mySecondMethod(string name)
+    {
+        print("Hell, your name is " + name);
+    }
+    
+}
